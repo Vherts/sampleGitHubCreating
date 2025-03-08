4bba3fb74434ca455e8295346a58a1315b7e764b
Vherts <137765947+Vherts@users.noreply.github.com>
2025-03-08 18:37:09 +0800

    Update Form1.Designer.cs Add Press me

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -50,7 +50,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(252, 85);
             this.button2.TabIndex = 0;
-            this.button2.Text = "Thank you";
+            this.button2.Text = "Press Me!";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button1_Click);
             // 
