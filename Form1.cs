326a90b4f0debb24a7c88495de8a7c1a32a2e677
Vherts <vhersarmiento77@gmail.com>
2025-03-08 18:07:59 +0800

    MessageBox update

--- Form1.cs
+++ Form1.cs
@@ -19,7 +19,7 @@ namespace sampleGitHubCreating
 
         private void button1_Click ( object sender, EventArgs e )
         {
-            MessageBox.Show("Hello World!");
+            MessageBox.Show("Hello World!", "System Alert", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
